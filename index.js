b60f63dbd4eeca47ec800ee69591bceefa10b171
e-oj <ooolaojo@gmail.com>
2024-08-31 01:57:15 -0700

    change indentation

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const React = require("react");
 const MagicGrid = require("magic-grid");
 
 const useMagicGrid = (props) => {
-    const gridRef = React.useRef(null);
-    const { container } = props;
+  const gridRef = React.useRef(null);
+  const {container} = props;
 
-    React.useEffect(() => {
-        if (!container) {
-            throw new Error("Container name or element is required");
-        }
+  React.useEffect(() => {
+    if (!container) {
+      throw new Error("Container name or element is required");
+    }
 
-        if (!gridRef.current) {
-            gridRef.current = new MagicGrid({ ...props, items: props.items || 1, static: false });
-            gridRef.current.listen();
-            return;
-        }
+    if (!gridRef.current) {
+      gridRef.current = new MagicGrid({...props, items: props.items || 1, static: false});
+      gridRef.current.listen();
+      return;
+    }
 
-        const grid = gridRef.current;
-        const currentContainer = document.querySelector(grid.containerClass);
-        const containerChanged = grid.container !== currentContainer;
+    const grid = gridRef.current;
+    const currentContainer = document.querySelector(grid.containerClass);
+    const containerChanged = grid.container !== currentContainer;
 
-        if (currentContainer && containerChanged){
-            grid.setContainer(currentContainer);
-        }
-    });
+    if (currentContainer && containerChanged) {
+      grid.setContainer(currentContainer);
+    }
+  });
 
-    return gridRef.current;
+  return gridRef.current;
 };
 
 module.exports = {
-    useMagicGrid
+  useMagicGrid
 }
--- stories/Grid.jsx
+++ stories/Grid.jsx
@@ -1,83 +1,81 @@
 import React from "react";
-import { useMagicGrid } from "../index";
+import {useMagicGrid} from "../index";
 import "./grid.css";
 
 const Grid = ({resizeable}) => {
-    const [name, setName] = React.useState("name")
+  const [name, setName] = React.useState("name")
 
-    React.useEffect(() => {
-        console.log(name);
+  React.useEffect(() => {
+    console.log(name);
 
-        if (name !== 'name'){
-            setTimeout(() => {
-                setName("name");
-            }, 5000);
-        }
-        else setTimeout(() => {
-            setName("name " + Math.random() % 10);
-        }, 10000);
+    if (name !== 'name') {
+      setTimeout(() => {
+        setName("name");
+      }, 5000);
+    } else setTimeout(() => {
+      setName("name " + Math.random() % 10);
+    }, 10000);
 
-    },[name]);
-
+  }, [name]);
 
 
-    const sharedProps = {
-        animate: true,
-        gutter: 30,
-        static: true,
-        useMin: true,
-    };
+  const sharedProps = {
+    animate: true,
+    gutter: 30,
+    static: true,
+    useMin: true,
+  };
 
-    const grid1 = useMagicGrid({
-        container: '.container',
-        ...sharedProps
-    });
+  const grid1 = useMagicGrid({
+    container: '.container',
+    ...sharedProps
+  });
 
-    const grid2 = useMagicGrid({
-        container: '.container2',
-        ...sharedProps
-    });
+  const grid2 = useMagicGrid({
+    container: '.container2',
+    ...sharedProps
+  });
 
-    console.log("grids", grid1);
-    console.log("grids", grid2);
+  console.log("grids", grid1);
+  console.log("grids", grid2);
 
-    return (
-        <>
-            {name === 'name' &&
-                <div className={`container ${resizeable ? 'resizeable' : ''}`} >
-                    <div className="item1">{`1 ${name}`}</div>
-                    <div className="item2">2</div>
-                    <div className="item3">3</div>
-                    <div className="item4">4</div>
-                    <div className="item5">5</div>
-                    <div className="item6">6</div>
-                    <div className="item7">7</div>
-                    <div className="item8">8</div>
-                    <div className="item9">9</div>
-                    <div className="item10">10</div>
-                    <div className="item11">11</div>
-                    <div className="item12">12</div>
-                    <div className="item13">13</div>
-                </div>
-            }
+  return (
+    <>
+      {name === 'name' &&
+        <div className={`container ${resizeable ? 'resizeable' : ''}`}>
+          <div className="item1">{`1 ${name}`}</div>
+          <div className="item2">2</div>
+          <div className="item3">3</div>
+          <div className="item4">4</div>
+          <div className="item5">5</div>
+          <div className="item6">6</div>
+          <div className="item7">7</div>
+          <div className="item8">8</div>
+          <div className="item9">9</div>
+          <div className="item10">10</div>
+          <div className="item11">11</div>
+          <div className="item12">12</div>
+          <div className="item13">13</div>
+        </div>
+      }
 
-            <div className="container2">
-                <div className="item1">1</div>
-                <div className="item2">2</div>
-                <div className="item3">3</div>
-                <div className="item4">4</div>
-                <div className="item5">5</div>
-                <div className="item6">6</div>
-                <div className="item7">7</div>
-                <div className="item8">8</div>
-                <div className="item9">9</div>
-                <div className="item10">10</div>
-                <div className="item11">11</div>
-                <div className="item12">12</div>
-                <div className="item13">13</div>
-            </div>
-        </>
-    );
+      <div className="container2">
+        <div className="item1">1</div>
+        <div className="item2">2</div>
+        <div className="item3">3</div>
+        <div className="item4">4</div>
+        <div className="item5">5</div>
+        <div className="item6">6</div>
+        <div className="item7">7</div>
+        <div className="item8">8</div>
+        <div className="item9">9</div>
+        <div className="item10">10</div>
+        <div className="item11">11</div>
+        <div className="item12">12</div>
+        <div className="item13">13</div>
+      </div>
+    </>
+  );
 };
 
 export default Grid;
